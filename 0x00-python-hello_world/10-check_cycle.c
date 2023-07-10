53cf82d13a7274030d76d432f68580a7cc5cbe0b
sharonkisenga <sharonglor69@gmail.com>
2023-07-10 05:28:07 -0700

    10ckeckcycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
  * check_cycle - checks if a singly linked list has a cycle
- * @list: listint_t 
+ * @list: listint_t
  * return: 0 if no cycle, 1 if there is a cycle
  */
 int check_cycle(listint_t *list)
@@ -13,7 +13,7 @@ int check_cycle(listint_t *list)
 		return (0);
 	pop = list;
 	check = pop->next;
-	while (pop != NULL && check->next != NULL && check->next->next!= NULL)
+	while (pop != NULL && check->next != NULL && check->next->next != NULL)
 	{
 	if (pop == check)
 	return (1);
