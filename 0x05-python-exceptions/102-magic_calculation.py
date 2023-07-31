e215e3215e1f224ddb1ca8c11b911c3536bd41a1
sharonkisenga <sharonglor69@gmail.com>
2023-07-31 06:26:08 -0700

    calcut102

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    attente = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception("Too far")
+            attente += (a ** b) / i
+        except Exception:
+            attente = b + a
+            break
+    return attente
