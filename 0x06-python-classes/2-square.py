0062134832af677eadac7235a87d701e81d897ba
sharonkisenga <sharonglor69@gmail.com>
2023-08-01 03:28:06 -0700

    task2py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,10 +2,15 @@
 """ defines a square by: (based on 1-square.py) """
 
 class Square:
+    """ define class square """
      def __init__(self, size=0):
+         """ define class square """
         if type(size) != int:
+            """ class square """
             raise TypeError("size must be an integer")
         elif size < 0:
+            """ class square """
             raise ValueError("size must be >= 0")
         else:
+            """ class square """
             self.__size = size
