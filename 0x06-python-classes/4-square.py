7bc85bb5f62b1f44c3f225cf2703a06cc9365955
sharonkisenga <sharonglor69@gmail.com>
2023-08-01 03:36:48 -0700

    task4py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,34 +1,29 @@
 #!/usr/bin/python3
+""" creates class Square """
+
+
 class Square:
-    """ defines a square by: (based on 3-square.py) """
+    """ Square class"""
     def __init__(self, size=0):
-        """ defines a square by: (based on 3-square.py) """
         if type(size) != int:
-            """ defines a square by: (based on 3-square.py) """
             raise TypeError("size must be an integer")
         elif size < 0:
-            """ defines a square by: (based on 3-square.py) """
             raise ValueError("size must be >= 0")
         else:
-            """ defines a square by: (based on 3-square.py) """
             self.__size = size
 
     @property
     def size(self):
-        """ defines a square by: (based on 3-square.py) """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """ defines a square by: (based on 3-square.py) """
         if type(value) != int:
             raise TypeError("size must be an integer")
         elif value < 0:
-            """ defines a square by: (based on 3-square.py) """
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
 
     def area(self):
-        """ defines a square by: (based on 3-square.py) """
         return self.__size * self.__size
