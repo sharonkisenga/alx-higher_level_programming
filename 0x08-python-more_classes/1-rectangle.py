eece46574dd514ddf1192b38483ddc15f71939b2
sharonkisenga <sharonglor69@gmail.com>
2023-08-07 03:34:49 -0700

    task1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""class rectangle"""
+class Rectangle:
+    """Class of a Rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Initialize class"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """find width"""
+        return self.__width
+
+    @width.setter
+    def width(self, widthValue):
+        """find width"""
+        if type(widthValue) != int:
+            raise TypeError("width must be an integer")
+        if widthValue < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = widthValue
+
+    @property
+    def height(self):
+        """find height"""
+        return self.__height
+
+    @height.setter
+    def height(self, HeightValue):
+        """find height"""
+        if type(HeightValue) != int:
+            raise TypeError("height must be an integer")
+        if HeightValue < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = HeightValue
