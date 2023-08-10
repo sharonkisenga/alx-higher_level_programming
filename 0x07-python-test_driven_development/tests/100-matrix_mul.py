7b94a0cd96d9c42c105701e2d8560e2e5fdde3f8
sharonkisenga <sharonglor69@gmail.com>
2023-08-10 01:57:36 -0700

    task0

--- 0x07-python-test_driven_development/tests/100-matrix_mul.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-def matrix_mul(m_a, m_b):
-~                            
--- 0x07-python-test_driven_development/tests/101-lazy_matrix_mul.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""use the module numpy"""
-import numpy
-def lazy_matrix_mul(m_a, m_b):
-    """multiply numpy""101-lazy_matrix_mul.py"
-    return numpy.matmul(m_a, m_b)
-~                                       
