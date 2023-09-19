8d33cb13d8b79a4950ee8e9fec0b180459ce82e2
sharonkisenga <sharonglor69@gmail.com>
2023-09-19 03:35:34 -0700

    task8

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,3 @@
---Write a script that displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL server.
+--Write a script that displays the number of records
+-- Count rows with id 89
 SELECT COUNT(*) FROM first_table WHERE id=89;
