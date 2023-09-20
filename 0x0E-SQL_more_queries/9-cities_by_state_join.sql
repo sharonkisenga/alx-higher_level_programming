65c4422562736746a568b38b2b49b27633dc81ad
sharonkisenga <sharonglor69@gmail.com>
2023-09-20 05:35:36 -0700

    task8

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,1 @@
--- Write a script that lists all cities contained in the database hbtn_0d_usa
-SELECT a.id AS id, a.name AS name, b.name AS name
-FROM cities a
-INNER JOIN states b
-ON a.state_id = b.id
-ORDER BY a.id ASC;
+-- Write a script that lists all cities contained in the database
