29b9c350853e624964442356077aa71cade674df
sharonkisenga <sharonglor69@mail.com>
2023-11-02 05:01:14 -0700

    peaki

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
 """ finds a peak in a list of unsorted integers"""
 def find_peak(list_of_integers):
-    """ find the peak """
-    k = list_of_integers
-    peaks = []
-    for e in range(1, len(list_of_integers)-1):
-        if k[e] > k[e-1] and k[e] > k[e+1]:
-            peaks.append(l[e])
-        if k[e] < k[e-1] and k[e] < k[e+1]:
-            peaks.append(k[e])
-        if k[e] == k[e-1] and k[e] == k[e+1]:
-            peaks.append(l[e])
-    if len(peaks) == 0:
-        return None
-    return max(peaks)
+    """find peak list """
+    peak_i = None
+    for ele in list_of_integers:
+        if peak_i is None or peak_i < ele:
+            peak_i = ele
+    return peak_i
